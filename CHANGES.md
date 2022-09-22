faa262e3e29e419fca63a30c691b60ace1e5d56b
Mark Farnsworth <farnsworth2008@gmail.com>
2022-09-22 02:22:39 +0000

    0.0.2 Changes; 3 of 3 completed
    - Added Echo resources for testing
    - Added `s3d-env-init` and `s3d-start` scripts
    - Added working FastAPI

--- CHANGES.md
+++ CHANGES.md
@@ -1,9 +1,11 @@
 # [Changes](https://www.s3d.club/changes/)
 Recently completed and planned work is tracked here.
 
-## [0.0.0](.)
-- Created
-
-## [0.0.1](.)
-- Added working FastAPI
+## [0.0.0](.) [0.0.2](.)
+- Added Echo resources for testing
 - Added `s3d-env-init` and `s3d-start` scripts
+- Added working FastAPI
+
+## [Next](.)
+- **TODO** Added secure header
+- **TODO** Added structured input/output
--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+import time
 
 app = FastAPI()
+
+
+@app.get("/demo/echo/seconds/{seconds}")
+async def echo_seconds(seconds: int):
+    time.sleep(seconds)
+    return {
+      "seconds": seconds,
+      "seconds_in_array": [seconds, seconds, seconds],
+      "seconds_with_string": str(seconds) + "_demo",
+    }
+
+
+@app.get("/demo/echo/request/{seconds}")
+async def echo_request(seconds: int, rq: Request):
+    time.sleep(seconds)
+    return {
+        "seconds": seconds,
+        "request": {
+            "client": {"host": rq.client.host},
+            "headers": rq.headers,
+            "query_params": rq.query_params,
+            "path_params": rq.path_params,
+            "cookies": rq.cookies,
+        },
+    }
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
